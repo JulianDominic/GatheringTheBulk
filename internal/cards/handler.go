60267b450df96194379bb92d0bc437947f8eb1ce
Julian <juliandominic@yahoo.com.sg>
2026-01-10 23:21:34 +0800

    chore: the rewrite

--- internal/cards/handler.go
+++
@@ -1,6 +0,0 @@
-package cards
-
-import "net/http"
-
-func AddCardHandler(w http.ResponseWriter, r *http.Request) {
-}
--- internal/constants/exitCodes.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-const (
-	EXT_ERR_SERVER_START = 1
-	EXT_ERR_DB_OPEN      = 2
-	EXT_ERR_DB_CONNECT   = 3
-)
